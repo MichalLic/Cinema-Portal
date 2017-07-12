2b5a206631a35c6f54c4c0c6d7968235a14186ba
Michal <m14lickiewicz@gmail.com>
2017-07-12 16:28:15 +0200

    Set routes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,28 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavComponent } from './nav/nav.component';
 import { MainComponent } from './main/main.component';
-import {CinemaService} from "./cinema.service";
+import {CinemaService} from './cinema.service';
+import {RouterModule, Routes} from '@angular/router';
+import { NewsComponent } from './news/news.component';
+
+export  const routes: Routes = [
+  {path: '', component: MainComponent, pathMatch: 'full'},
+  {path: 'news', component: NewsComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavComponent,
-    MainComponent
+    MainComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [CinemaService],
   bootstrap: [AppComponent]
