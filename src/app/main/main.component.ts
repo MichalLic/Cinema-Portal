63a20a89de3412d6518ae5f24d86a49d35593bde
Michal <m14lickiewicz@gmail.com>
2017-07-13 13:47:57 +0200

    Set images view.

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -8,9 +8,21 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    console.log(this.viewImg);
+  }
+
+  viewImg = [
+    {url: '../../assets/images/detail-img1.jpeg'},
+    {url: '../../assets/images/detail-img2.jpeg'},
+    {url: '../../assets/images/detail-img3.jpeg'},
+    {url: '../../assets/images/detail-img4.jpeg'},
+    {url: '../../assets/images/detail-img5.jpeg'},
+    {url: '../../assets/images/detail-img6.jpeg'},
+    {url: '../../assets/images/detail-img7.jpeg'},
+    {url: '../../assets/images/detail-img8.jpeg'}
+  ];
 }
