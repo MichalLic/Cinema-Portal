922f6935b1f860ffcda828a5562beff3fb79e412
Michal <m14lickiewicz@gmail.com>
2017-07-17 14:24:37 +0200

    Reservation subpage in progress.

--- src/app/reservation/reservation.component.ts
+++ src/app/reservation/reservation.component.ts
@@ -11,10 +11,25 @@ export class ReservationComponent implements OnInit {
   films;
   selectedFilm;
   dateFilm;
-  constructor(private cinemaService: CinemaService) { }
+  seatSeries = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+  seatRows = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
+  seatCounter = 0;
+
+  constructor(private cinemaService: CinemaService) {
+  }
 
   ngOnInit() {
-  this.films = this.cinemaService.getFilmDetails();
+    this.films = this.cinemaService.getFilmDetails();
+  }
+
+  reservedSeat(seat) {
+    seat.target.classList.remove('reserved');
+    this.seatCounter--;
+  }
+
+  unreservedSeat(seat) {
+    seat.target.classList.add('reserved');
+    this.seatCounter++;
   }
 
 }
